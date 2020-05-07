945a58dcd95148d51a7d6fbdabff377a8d44cd1e
dbh <david@davidbharrison.com>
2020-05-06 21:39:16 -0700

    init commit

---
+++ src/main/java/com/davidbharrison/restfun/num_to_english/NumToEnglishApplication.java
@@ -0,0 +1,13 @@
+package com.davidbharrison.restfun.num_to_english;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NumToEnglishApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NumToEnglishApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/davidbharrison/restfun/num_to_english/NumToEnglishApplicationTests.java
@@ -0,0 +1,13 @@
+package com.davidbharrison.restfun.num_to_english;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NumToEnglishApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
