9c4a4c38f7becb0682fc05997ff9c73612e9c8dc
dbh <david@davidbharrison.com>
2020-05-08 15:08:49 -0700

    comments

--- src/main/java/com/davidbharrison/restfun/num_to_english/controller/NumToEnglishController.java
+++ src/main/java/com/davidbharrison/restfun/num_to_english/controller/NumToEnglishController.java
@@ -18,25 +18,28 @@ public class NumToEnglishController {
         this.numToEnglishService = numToEnglishService;
     }
 
-
-    @GetMapping(value="/num_to_english_better", produces = {"application/json"})
-    public NumToEnglishResponse getConversionBetter(@RequestParam String number) {
-        log.info("getConversion the right way "+number);
-        return numToEnglishService.converNumToEnglish(number);
-    }
-
     @GetMapping(value="/num_to_english", produces = {"application/json"})
     public NumToEnglishResponse getConversion(@RequestBody NumToEnglishRequest req) {
-        log.info("getConversion "+req.toString());
-        // rfc2616, section-4.3
-        // Illegal per HTTP spec, but technically possible. Don't use GET to send a Payload...
-        // if you must use a GET, perhaps put the number on the URL as a parameter
+        log.info("GET num_to_english getConversion "+req.toString());
+        // It's bad practice and not guaranteed to be supported to send a message
+        // body on a GET request. If you must use a GET, perhaps put the number on the
+        // URL as a parameter. See /num_to_english_param instead.
         return numToEnglishService.converNumToEnglish(req.getNumber());
+    }
+
+    @GetMapping(value="/num_to_english_param", produces = {"application/json"})
+    public NumToEnglishResponse getConversionBetter(@RequestParam String number) {
+        log.info("GET num_to_english_param getConversion the right way "+number);
+        // This wasn't explicitly requested, but it is better practice.
+        return numToEnglishService.converNumToEnglish(number);
     }
 
     @PostMapping(value="/num_to_english", produces = {"application/json"})
     public NumToEnglishResponse postConversion(@RequestBody  NumToEnglishRequest req) {
-        log.info("postConversion "+req.toString());
+        log.info("POST num_to_english postConversion "+req.toString());
+        // Also this wasn't requested, but is a correct way to send a message body for processing
+        // However, a POST, can be misleading because this request is not actually creating a new
+        // entity, but rather just passing data for transformation and getting it back
         return numToEnglishService.converNumToEnglish(req.getNumber());
     }
 }
